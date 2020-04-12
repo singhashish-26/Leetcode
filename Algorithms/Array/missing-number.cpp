5fc0f6490ef18ba734a3db13ff1da59cbcc4c498
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-04-12 12:51:15 +0530

    Create missing-number.cpp

---
+++ Algorithms/Array/missing-number.cpp
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        int s = n*(n+1);
+        s/=2;
+        for(int i=0; i<n; i++)
+        {
+            s-=(nums[i]);
+        }
+        return s;
+    }
+};
