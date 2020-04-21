3082bc1954a24763dee8f7400db46a58217ca60b
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-04-21 23:58:39 +0530

    Create flatten-a-multilevel-doubly-linked-list.cpp

---
+++ Algorithms/Linked List/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,48 @@
+#https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* fun(Node* temp)
+    {
+        
+        while(temp->next!=NULL && temp->child==NULL)
+        {
+            temp=temp->next;
+        }
+        Node* x= temp->next;
+        //Node* pre= temp;
+        if(temp->next==NULL && temp->child==NULL)
+        {
+            return temp;
+        }
+        temp->child->prev=temp;
+        temp->next=temp->child;
+        temp->child=NULL;
+        Node* y = fun(temp->next);
+        y->next=x;
+        if(x!=NULL)
+            x->prev=y;
+        temp=x;
+        return fun(temp);
+    }
+    
+    Node* flatten(Node* head) {
+        if(head==NULL)
+            return head;
+        Node* temp = head;
+        temp = fun(temp);
+        temp= head;
+        return temp;
+    }
+};
