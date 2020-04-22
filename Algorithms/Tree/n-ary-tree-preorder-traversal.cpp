1bb3628f5538d445042b325f54441b1f051948ab
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-04-22 21:14:20 +0530

    Create n-ary-tree-preorder-traversal.cpp

---
+++ Algorithms/Tree/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,46 @@
+#https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    
+    void fun(Node* root)
+    {
+        if(root==NULL)
+            return;
+        int i=0;
+        ans.push_back(root->val);
+        while(i<root->children.size())
+        {
+            fun(root->children[i]);
+            i++;
+        }
+    }
+    
+    vector<int> preorder(Node* root) {
+        fun(root);
+        if(ans.size())
+            return ans;
+        return {};
+    }
+};
