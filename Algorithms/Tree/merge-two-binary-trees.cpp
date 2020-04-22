c7a8cdde528cc2f7c6a25e31b9bb7ebcaab96348
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-04-22 14:36:11 +0530

    Update merge-two-binary-trees.cpp

--- Algorithms/Tree/merge-two-binary-trees.cpp
+++ Algorithms/Tree/merge-two-binary-trees.cpp
@@ -11,40 +11,14 @@
  */
 class Solution {
 public:
-    TreeNode* newNode(int x)
-    {
-        TreeNode* temp = new TreeNode();
-        temp->val=x;
-        temp->left=NULL;
-        temp->right=NULL;
-        return temp;
-    }
-    
-    TreeNode* merge(TreeNode* t1, TreeNode* t2)
-    {
-        int x = t1->val + t2->val;
-        TreeNode* temp = newNode(x);
-        if(t1->left!=NULL && t2->left!=NULL)
-            temp->left= merge(t1->left, t2->left);
-        if(t1->left==NULL && t2->left!=NULL)
-            temp->left=t2->left;
-        if(t1->left!=NULL && t2->left==NULL)
-            temp->left=t1->left;
-        if(t1->right!=NULL && t2->right!=NULL)
-            temp->right= merge(t1->right, t2->right);
-        if(t1->right==NULL && t2->right!=NULL)
-            temp->right=t2->right;
-        if(t1->right!=NULL && t2->right==NULL)
-            temp->right=t1->right;
-        return temp;
-    }
-    
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
         if(t1==NULL)
             return t2;
         if(t2==NULL)
             return t1;
-        return merge(t1,t2);
-        
+        t1->val+=t2->val;
+        t1->left= mergeTrees(t1->left,t2->left);
+        t1->right= mergeTrees(t1->right,t2->right);
+        return t1;
     }
 };
