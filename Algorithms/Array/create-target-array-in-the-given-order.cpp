ddb1f9be2183853d407a5289f27244c429c42f47
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-06-08 00:30:16 +0530

    Create create-target-array-in-the-given-order.cpp

---
+++ Algorithms/Array/create-target-array-in-the-given-order.cpp
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> ans;
+        for(int i=0; i<index.size(); i++)
+        {
+            ans.insert(ans.begin()+index[i], nums[i]);
+        }
+        return ans;
+    }
+};
