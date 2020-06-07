cb48136f92a4155fb7a459ae69f96002b7b55c64
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-06-07 23:58:38 +0530

    Create shuffle-the-array.cpp

---
+++ Algorithms/Array/shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/shuffle-the-array/
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans;
+        for(int i=0,j=n; i<n; i++,j++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+        }
+        return ans;
+    }
+};
