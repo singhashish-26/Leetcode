a96c256b3f0918b7f61a15fff64d3d1ec7833989
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-06-09 23:53:26 +0530

    Create minimum-absolute-difference.cpp

---
+++ Algorithms/Array/minimum-absolute-difference.cpp
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/minimum-absolute-difference/
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int mn=INT_MAX;
+        for(int i=0; i<arr.size()-1; i++)
+        {
+            mn = min(mn, abs(arr[i]-arr[i+1]));
+        }
+        vector<vector<int>> ans;
+        for(int i=0; i<arr.size()-1; i++)
+        {
+            if(abs(arr[i]-arr[i+1])==mn)
+            {
+                ans.push_back({arr[i],arr[i+1]});
+            }
+        }
+        return ans;
+    }
+};
