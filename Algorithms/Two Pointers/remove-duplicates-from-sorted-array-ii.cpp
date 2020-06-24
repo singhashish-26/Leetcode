4e0d9c70a403e264e62f93591f6e45e7ee55c1cb
Ashish Singh <63361457+singhashish-26@users.noreply.github.com>
2020-06-24 22:09:55 +0530

    Create remove-duplicates-from-sorted-array-ii.cpp

---
+++ Algorithms/Two Pointers/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=2)
+            return n;
+        int pos=2;
+        for(int i=2; i<n; i++)
+        {
+            if(nums[i]!=nums[pos-2])
+                nums[pos++]=nums[i];
+        }
+        return pos;
+    }
+};
